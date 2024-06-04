fdac23d6facbc2355f05c53c588594b83be742e1
RaminTuzelis <k.mickunas@gmail.com>
2024-06-04 20:15:22 +0300

    Configuring Laravel, Vue and Inertia.Js Test index.vue for counting numbers.

---
+++ resources/js/Pages/Index/index.vue
@@ -0,0 +1,18 @@
+<template>Hello ! {{ counter }}</template>
+
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+    setup() {
+        const counter = ref(0)
+
+        onMounted(() => {
+            setInterval(() => counter.value++, 1000)
+        })
+
+        return { counter }
+    }
+}
+</script>
