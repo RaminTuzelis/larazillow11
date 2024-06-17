dad9646bdee5f1154dcccd57ed7934e5de87b7d9
RaminTuzelis <k.mickunas@gmail.com>
2024-06-17 19:43:24 +0300

    Persistent Layout

--- resources/js/Pages/Index/show.vue
+++ resources/js/Pages/Index/show.vue
@@ -1,12 +1,17 @@
 <template>
-    <MainLayout>
         <div>Show</div>
         <Link href="/">Main Page</Link>
-    </MainLayout>
 </template>
 
 
 <script setup>
 import {Link} from "@inertiajs/vue3";
-import MainLayout from "@/Layouts/MainLayout.vue";
+
+</script>
+
+<script>
+import MainLayout from '../../Layouts/MainLayout.vue';
+export default {
+    layout: MainLayout
+}
 </script>
